e6bab849a9802c7765b687bea36f420a76338688
bilalshahid929 <bilalshahid929@gmail.com>
2025-02-05 03:28:20 +0500

    feat: add the video conrolls

---
+++ js/videoControls.js
@@ -0,0 +1,12 @@
+function toggleVideoPlayback() {
+  var video = document.querySelector("#infoVideo");
+  var playButton = document.querySelector(".video-block__play-button");
+  console.log("func called");
+  if (video.paused) {
+    video.play();
+    playButton.classList.add("playing");
+  } else {
+    video.pause();
+    playButton.classList.remove("playing");
+  }
+}
